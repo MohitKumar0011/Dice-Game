b8e5045493570b48d428bb1fd1516ed995480fe3
Mohit Kumar <97847250+MohitKumarHans@users.noreply.github.com>
2022-12-22 22:44:33 +0530

    Add initial Dice Game files

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.floor(Math.random()*6)+1; // 1-6
+var randomImageSource1 = "images/dice"+randomNumber1+".png"; // images/dice1.png - images/dice6.png
+document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1; // 1-6
+var randomImageSource2 = "images/dice"+randomNumber2+".png"; // images/dice1.png - images/dice6.png
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+
+// if player 1 wins
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Won";
+
+}
+// if player 2 wins
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 Won";
+}
+// if there is a draw
+else{
+    document.querySelector("h1").innerHTML="Draw";
+}
+
